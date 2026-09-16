3a2d2c355a43cdf91aec3a1a82802098d71445c1
5586663 <mtwrhu500j2h@imageeditgpt.com>
2026-09-16 12:47:09 +0800

    feat: fuse Download allow app/src/main/java/com/piliplus/export/MainHook.java

--- app/src/main/java/com/piliplus/export/MainHook.java
+++ app/src/main/java/com/piliplus/export/MainHook.java
@@ -56,10 +56,7 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lp) {
-        if ("android".equals(lp.packageName)) {
-            StorageHook.hookSystem(lp.classLoader);
-            return;
-        }
+        if ("android".equals(lp.packageName)) return;
         if (lp.packageName != null && lp.packageName.startsWith("com.android.providers.media")) {
             StorageHook.hookMediaProvider(lp.classLoader);
             return;
